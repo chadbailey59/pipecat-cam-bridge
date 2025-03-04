18ca79f8a0aa5a4e565a21b6bb3f9f180fe5fbc9
Chad Bailey <chadbailey@gmail.com>
2025-03-04 15:47:11 +0000

    initial import

--- bot.py
+++ bot.py
@@ -9,15 +9,9 @@ import os
 import aiohttp
 from dotenv import load_dotenv
 from loguru import logger
-
-from pipecat.audio.vad.silero import SileroVADAnalyzer
-from pipecat.frames.frames import LLMMessagesFrame
 from pipecat.pipeline.pipeline import Pipeline
 from pipecat.pipeline.runner import PipelineRunner
 from pipecat.pipeline.task import PipelineParams, PipelineTask
-from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext
-from pipecat.services.cartesia import CartesiaTTSService
-from pipecat.services.openai import OpenAILLMService
 from pipecat.transports.services.daily import DailyParams, DailyTransport
 
 load_dotenv(override=True)
@@ -35,36 +29,15 @@ async def main(room_url: str, token: str, session_logger=None):
             "bot",
             DailyParams(
                 audio_out_enabled=True,
-                transcription_enabled=True,
-                vad_enabled=True,
-                vad_analyzer=SileroVADAnalyzer(),
+                video_out_enabled=True,
+                transcription_enabled=False,
+                vad_enabled=False,
             ),
         )
 
-        tts = CartesiaTTSService(
-            api_key=os.getenv("CARTESIA_API_KEY"), voice_id="79a125e8-cd45-4c13-8a67-188112f4dd22"
-        )
-
-        llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o")
-
-        messages = [
-            {
-                "role": "system",
-                "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way.",
-            },
-        ]
-
-        context = OpenAILLMContext(messages)
-        context_aggregator = llm.create_context_aggregator(context)
-
         pipeline = Pipeline(
             [
-                transport.input(),
-                context_aggregator.user(),
-                llm,
-                tts,
                 transport.output(),
-                context_aggregator.assistant(),
             ]
         )
 
@@ -81,15 +54,6 @@ async def main(room_url: str, token: str, session_logger=None):
         @transport.event_handler("on_first_participant_joined")
         async def on_first_participant_joined(transport, participant):
             log.info("First participant joined: {}", participant["id"])
-            await transport.capture_participant_transcription(participant["id"])
-            # Kick off the conversation.
-            messages.append(
-                {
-                    "role": "system",
-                    "content": "Please start with 'Hello World' and introduce yourself to the user.",
-                }
-            )
-            await task.queue_frames([LLMMessagesFrame(messages)])
 
         @transport.event_handler("on_participant_left")
         async def on_participant_left(transport, participant, reason):
@@ -129,20 +93,19 @@ async def bot(config, room_url: str, token: str, session_id=None, session_logger
 LOCAL_RUN = os.getenv("LOCAL_RUN")
 if LOCAL_RUN:
     import asyncio
-    import webbrowser
-
-    from local_runner import configure
 
 
 async def local_main():
     async with aiohttp.ClientSession() as session:
-        (room_url, token) = await configure(session)
+        # (room_url, token) = await configure(session)
+        room_url = os.getenv("DAILY_ROOM")
+        token = os.getenv("DAILY_TOKEN")
         logger.warning("_")
         logger.warning("_")
         logger.warning(f"Talk to your voice agent here: {room_url}")
         logger.warning("_")
         logger.warning("_")
-        webbrowser.open(room_url)
+        # webbrowser.open(room_url)
         await main(room_url, token)
 
 
